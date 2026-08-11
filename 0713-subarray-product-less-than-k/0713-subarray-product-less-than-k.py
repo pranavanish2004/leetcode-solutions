905915851d7a8cc44c61999b7eb74bba39aef7f7
Pranav Anish <pranavanish2004@gmail.com>
2026-08-11 17:17:30 +0530

    Time: 18 ms (98.73%), Space: 21.4 MB (70.06%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -4,7 +4,7 @@ class Solution:
         temp=1
         ans=0
         if k<=1:
-            return 0
+            return 0  #stricty less than product if k=1 also the answer should less than 1
         for r in range(len(nums)):
             temp=temp*nums[r]
             while(temp>=k):
