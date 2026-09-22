3b750b50e36be177aa5fad6bf69f6f2295c74df8
Pranav Anish <pranavanish2004@gmail.com>
2026-09-22 12:46:48 +0530

    Time: 10 ms (98.22%), Space: 20.8 MB (66.6%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,31 +1,13 @@
 from collections import Counter
-from collections import deque
-import heapq
 class Solution:
     def leastInterval(self, tasks: list[str], n: int) -> int:
-        #count the tasks
-        dici=Counter(tasks)
-        
-        heap=[]
-        #max heap
-        for task in dici:
-            heapq.heappush(heap,-dici[task])
-        #queue for tasks to cooldown
-        queue=deque()
-        time=0
-        while len(heap)>0 or len(queue)>0:
-            time+=1
-            if len(heap)>0:
-                freq=heapq.heappop(heap)
-                freq+=1
-                #if task still has remaining exections
-                if freq!=0:
-                    queue.append((freq,time+n))
-            
-            #check whether a task has completed its cooldown
-            if len(queue)>0:
-                if queue[0][1]==time:
-                    freq,available_time=queue.popleft()
-                    heapq.heappush(heap,freq)
-        return time
-        
+        count=Counter(tasks)
+        #find max frequnecy
+        max_freq=max(count.values())
+        count_max_freq=0
+        for i in count.values():
+            if(i==max_freq):
+                count_max_freq+=1
+        #calculate minimum intervals
+        ans=(max_freq-1)*(n+1) +count_max_freq
+        return max(len(tasks),ans)
